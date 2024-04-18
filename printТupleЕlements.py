ec35897c19152085d3e2bd35636eb8d8b7a97b81
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-18 20:04:48 +0300

    Create printТupleЕlements.py

---
+++ printТupleЕlements.py
@@ -0,0 +1,8 @@
+def printТupleЕlements(t):
+    for element in t:
+        print(element)
+
+myTuple = (6, 'ivan', 'petur', 12, True)
+
+print("Елементите на кортежа са:")
+printТupleЕlements(myTuple)
